178ed02f0e5f4485522a31ead96cbd82f94a036c
wolfguoliang <wolfguoliang@gmail.com>
2016-06-07 09:11:58 +0800

    建表语句

---
+++ ansible.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `ansible_host` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hostname` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `manufacturer` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `os` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `cpu_model` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `cpu_count` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `cpu_core` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `memory_totally` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `swap_totally` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `disk` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `ip` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `mac_address` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `sn` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
